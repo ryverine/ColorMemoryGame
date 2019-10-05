3d57513bd7c474d42b50b39662f66c7657f85821
ryverine <rwcheek@gmail.com>
2019-10-05 19:55:27 -0400

    Replace root content with content from build folder.

--- README.md
+++
@@ -1,6 +0,0 @@
-# ColorMemoryGame
-New version of Color Memory Clicky Game built with React.
-
-Click each color only once!
-
-Deployed App: https://ryverine.github.io/ColorMemoryGame/
---
+++ precache-manifest.d7decc4778b4c91d2b48158fe304f4ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faaead77ff596c47229c8fc13d8cd1a0",
+    "url": "/ColorMemoryGame/index.html"
+  },
+  {
+    "revision": "41c31c38dec1419e79ab",
+    "url": "/ColorMemoryGame/static/css/main.1a69ee3b.chunk.css"
+  },
+  {
+    "revision": "fc9de1a157ea8a9493e6",
+    "url": "/ColorMemoryGame/static/js/2.a4f70863.chunk.js"
+  },
+  {
+    "revision": "41c31c38dec1419e79ab",
+    "url": "/ColorMemoryGame/static/js/main.de82edb7.chunk.js"
+  },
+  {
+    "revision": "4e3c0d49cdbd8ed436bf",
+    "url": "/ColorMemoryGame/static/js/runtime-main.78242950.js"
+  }
+]);
