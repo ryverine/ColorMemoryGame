270c28cb8acd8fbed4de80ffbf3337b4beeef1f7
ryverine <rwcheek@gmail.com>
2019-10-05 19:22:35 -0400

    Original version of color memory game.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,150 @@
+import React, { Component } from "react";
+import ColorCard from "./components/ColorCard";
+import Wrapper from "./components/Wrapper";
+import GameOver from "./components/GameOver";
+import {Title, Subtitle} from "./components/Title";
+import Score from "./components/Score";
+import colors from "./colors.json";
+
+class App extends Component {
+  state = {
+    pickedColors: [],
+    score: 0,
+    gameOver: false,
+    colors 
+  };
+
+  shuffleColorCards = () =>{
+    let curCards = this.state.colors;
+    let newCards = [];
+ 
+    while (newCards.length < 20)
+    {
+      var curCardIndex = Math.floor(Math.random()*curCards.length);
+      var testCard = curCards[curCardIndex];
+      var foundCard = false;
+      for (var i = 0; i < newCards.length; i++)
+      {
+        if(testCard.id === newCards[i].id)
+        {
+          foundCard = true;
+          break;
+        }
+      }
+
+      if(!foundCard)
+      {
+        newCards.push(testCard);
+      }
+    }
+
+    return newCards;
+  }
+  
+  colorPicked = id => {
+    const tmpPickedColors = this.state.pickedColors;
+    let tmpScore = this.state.score;
+    
+    if(tmpPickedColors.includes(id))
+    {
+      this.setState({gameOver: true});
+    }
+    else
+    {
+      tmpPickedColors.push(id);
+      tmpScore++;
+      this.setState({ 
+        pickedColors: tmpPickedColors,
+        score: tmpScore
+      });
+
+    }
+    
+    var shuffledColorCards = this.shuffleColorCards();
+
+    this.setState({ colors: shuffledColorCards });
+
+  };
+
+  startGame = () => {
+    console.log("START GAME");
+
+    var colorList = [];
+    var userScore = 0;
+    var isGameOver = false;
+    var shuffledColorCards = this.shuffleColorCards();
+
+    this.setState({ 
+      pickedColors: colorList,
+      score: userScore,
+      gameOver: isGameOver,
+      colors: shuffledColorCards });
+  };
+
+  render() {
+    if (this.state.gameOver)
+    {
+      return(
+        <Wrapper>
+          <Title>Color Memory Clicky Game</Title>
+          <GameOver 
+            title="GAME OVER!" 
+            messageText="Maybe next time, chief!" 
+            image="images/img_you_lose.png"
+            score={this.state.score}
+            startGame={this.startGame} /> 
+        </Wrapper>
+      );
+    }
+    else
+    {
+      if (this.state.score >= 20)
+      {
+        return(
+          <Wrapper>
+            <Title>Color Memory Clicky Game</Title>
+            <GameOver 
+              title="YOU WON" 
+              messageText="You did so good!" 
+              image="images/img_you_won.png"
+              score={this.state.score}
+              startGame={this.startGame} /> 
+          </Wrapper>
+        );
+      }
+      else
+      {
+        return (
+          <React.Fragment>
+            <Title>Color Memory Clicky Game</Title>
+            <Subtitle>Try to pick each color only once!</Subtitle>
+            <Score>{this.state.score}</Score>
+            <Wrapper>
+            {this.state.colors.map(color => (
+              <ColorCard
+                colorPicked={this.colorPicked}
+                key={color.id}
+                id={color.id}
+                name={color.name}
+                image={color.image}
+              />
+            ))}
+          </Wrapper>
+          </React.Fragment>
+        );
+      }
+    }
+  }
+
+}
+
+export default App;
+
+
+/*
+Default create-react-app
+
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -24,3 +171,9 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+*/
